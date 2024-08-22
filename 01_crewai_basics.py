f8d9bff8a359a409fbb53fd28a0b4ed10784ba62
Jun <oowtl7425@gmail.com>
2024-08-22 17:29:06 +0900

    first commit

---
+++ 01_crewai_basics.py
@@ -0,0 +1,60 @@
+from crewai import Crew, Agent, Task
+from langchain_ollama import ChatOllama
+# from langchain_openai import ChatOpenAI
+
+# 뭘로 실행할지 정해주는 것
+llm = ChatOllama(
+  # model 이름 넣어주기
+  model = 'phi3:3.8b',
+  # api key 를 넣어주거나 base_url을 넣어주거나.
+  base_url='http://127.0.0.1:11434'
+)
+
+# Crew : 조직원 => N명 (조직)
+# Agent : 요원 => 1명 (조직원)
+# Task : 미션
+
+user_question = input('편하게 질문해주세요 : )')
+
+# 쇼핑몰
+book_agent = Agent(
+  role='책 구매 어시스턴트',
+  goal='고객이 어떤 상황인지 설명을 하면 해당 상황에 맞는 우리 서점에 있는 책을 소개합니다.',
+  backstory='당신은 우리 서점의 모든 책 정보를 알고있으며, 사람들의 상황에 맞는 책을 소개하는데 전문가입니다.',
+  # 상품 정보는 나중에 REST API 로 넘겨줘야한다.
+  llm=llm
+)
+
+review_agent = Agent(
+  role='책 리뷰 어시스턴트',
+  goal='추천받은 책들의 도서에 대한 리뷰를 제공하고, 해당도서에 대한 심도있는 평가를 제공합니다.',
+  backstory='당신은 우리 서점의 모든 책 정보를 알고 있으며, 추천받은 책에 대한 전문가 수준의 리뷰를 제공합니다.',
+  llm=llm
+)
+
+# task 는 계속 늘릴 수 있다. (비용이 커질 뿐)
+recommend_book_task = Task(
+  # description='고객의 상황에 맞는 최고의 추천 도서 제안하기',
+  description=user_question,  #input 으로 받을 수도 있다.
+  expected_output='고객의 상황에 맞는 5개의 도서를 추천하고 해당 도서를 추천한 이유를 알려줘',
+  agent=book_agent,
+  output_file='recommend_book_task.md'
+)
+
+review_task = Task(
+  description='고객이 선택한 책에 대한 리뷰를 제공합니다.',
+  expected_output='고객이 선택한 책에 대한 리뷰를 제공합니다.',
+  agent=review_agent,
+  output_file='review_task.md'
+)
+
+# 요원과 미션을 관리한다.
+crew = Crew(
+  agents=[book_agent, review_agent],
+  tasks=[recommend_book_task, review_task],
+  verbose=True
+)
+
+result = crew.kickoff()
+
+print(result)
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # j-llm-bot
+
 LLM chat bot to use Crew AI
+
+## 1. 프로젝트 세팅
+
+## 2. 프로젝트 구성
+
+- 가상환경 설정 => Docker Container 설정 (개념 : 공간을 분리해서 따로 관리하겠다.)
+- python 3.8으로 개발을 했는데 배포할 서버가 3.3 버전이라면??
+  - 이건.. 배포할 때 알 수 있다..
+  - 그런데 이걸 로컬에서 작업하는 환경과 호스트 서버에서 작업하는 환경을 일치시켜줘야 한다.
+    - 이 작업을 Docker에서 하는 것이 맞지만.. 이번에는 venv 에서 하는 것으로 한다.
+
+```terminal
+# 가상환경 실행
+python3.12 -m venv .venv
+
+# 실행 mac
+source .venv/bin/activate
+```
+
+## 3. 프로젝트
+
+### 3-1. Ollama 모델 + CrewAI
+
+(1) Ollama 다운로드
+(2) ollama 를 통해서 llm 다운로드
+
+> ollama pull llama3.1
+> ollama pull phi3:3.8b
+>
+> ollama run phi3:3.8b
+
+### 3-2. CrewAI 사용하기
+
+- 언어 모델의 API 를 관리해준다.
+
+> pip install crewai crewai-tools
+> pip install langchain-ollama
+
+1. 실행순서 관리
+
+### 3-2. Flask 사용해서 기본적인 챗봇
+
+# RAG : 확장하는 기능 => PDF, DB 에서 들고와서 답변하는 기능
+
+DB 와 연동 가능, 외부 API 불러와서 사용 가능
